f69d08282c2571bbdfd28551bea4243488a1dfd6
Mitul Mistry <mitulmistrydev@gmail.com>
2016-06-11 04:49:58 +0000

    started thinking about angular routes

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,1 +1,27 @@
 var app = angular.module('app', ['ui.router']); //'ui.bootstrap' not working
+
+// app.config(function($stateProvider, $urlRouterProvider){
+//   $stateProvider
+//     .state('home', {
+//       url: '/',
+//       templateUrl: 'home.html',
+//       controller: 'HomeController as ctrl'
+//     });
+//
+//     $urlRouterProvider.otherwise('/'); //default route
+// });
+
+// routes:
+// -Not logged in:
+// 	-login
+// 	-register
+// -Others:
+// 	-home page - show all sketches - filter by tags
+// 	-user show pages - profile + sketches
+// 		-Your profile
+// 	-user forms:
+// 		-edit profile
+// 	-sketch show pages
+// 	-sketch forms:
+// 		-new
+// 		-edit + delete?
