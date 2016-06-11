bb8cffc4873154d2be7cde7f7814e92df29b65b8
Mitul Mistry <mitulmistrydev@gmail.com>
2016-06-11 17:14:46 +0000

    stubbed out basic ui-router routes

--- app/assets/javascripts/angular-app/app.js
+++ app/assets/javascripts/angular-app/app.js
@@ -1,15 +1,56 @@
-var app = angular.module('app', ['ui.router']); //'ui.bootstrap' not working
+var app = angular //set angular application to variable to be accessed later
+  .module('app', [ //define module and includ dependencies
+    'ui.router', //for routing
+    'templates', //include templates folder
+    'ngResource' //allow use of $resource
+    //'ui.bootstrap', //NOT WORKING
+  ]);
 
-// app.config(function($stateProvider, $urlRouterProvider){
-//   $stateProvider
-//     .state('home', {
-//       url: '/',
-//       templateUrl: 'home.html',
-//       controller: 'HomeController as ctrl'
-//     });
-//
-//     $urlRouterProvider.otherwise('/'); //default route
-// });
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+    .state('home', { //create a ui-router state
+      url: '/', //set url route
+      templateUrl: 'home.html', //set template url
+      controller: 'HomeController as ctrl' //set controller for this route (equavelent of using <ng-controller="..."> in template)
+    })
+    .state('login', {
+      url: '/login',
+      templateUrl: 'auth/login.html',
+      controller: 'AuthController'
+    })
+    .state('register', {
+      url: '/register',
+      templateUrl: 'auth/register.html',
+      controller: 'AuthController'
+    })
+    .state('artist', {
+      url: '/artists/:id',
+      templateUrl: 'artists/show.html',
+      controller: 'ArtistController as ctrl'
+    })
+    .state('editArtist', {
+      url: '/artists/:id/edit',
+      templateUrl: 'artists/edit.html',
+      controller: 'EditArtistController as ctrl'
+    })
+    .state('newSketch', {
+      url: '/sketches/new',
+      templateUrl: 'sketches/new.html',
+      controller: 'NewSketchController as ctrl'
+    })
+    .state('sketch', {
+      url: '/sketches/:id',
+      templateUrl: 'sketches/show.html',
+      controller: 'SketchController as ctrl'
+    })
+    .state('editSketch', {
+      url: '/sketches/:id/edit',
+      templateUrl: 'sketches/edit.html',
+      controller: 'EditSketchController as ctrl'
+    });
+
+    $urlRouterProvider.otherwise('/'); //default route
+});
 
 // routes:
 // -Not logged in:
