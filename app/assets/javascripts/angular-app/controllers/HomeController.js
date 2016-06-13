4955d10d938f0b9f89d80450831ea8e29f271021
Mitul Mistry <mitulmistrydev@gmail.com>
2016-06-13 04:31:53 +0000

    modified home page

--- app/assets/javascripts/angular-app/controllers/HomeController.js
+++ app/assets/javascripts/angular-app/controllers/HomeController.js
@@ -1,6 +1,10 @@
-function HomeController() {
+function HomeController($state) {
   var ctrl = this;
+  ctrl.$state = $state;
 
+  ctrl.home = function(){ //checks whether the current page is home
+    return $state.current.name === 'home';
+  }
 }
 
 angular
