990b2ae6f9c7444cb51199455b9cd2f5485d1198
Mitul Mistry <mitulmistrydev@gmail.com>
2016-06-19 16:07:05 +0000

    created TagsService

---
+++ app/assets/javascripts/angular-app/services/TagsService.js
@@ -0,0 +1,35 @@
+function TagsService($http) {
+  this.getTags = function () { //get all the tags from the serialized controller action
+    return $http.get('/tags.json');
+  }
+
+  this.getTag = function(id) { //get the tag by id from the serialized controller action
+    return $http.get('/tags/' + id + '.json');
+  }
+
+  this.createTag = function(tag) {
+    var data = {
+      tag: { //rails strong params expects tag model
+        name: tag.name
+      }
+    };
+
+    $http({
+      method: 'POST',
+      dataType: 'json',
+      url: '/tags',
+      data: data,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(function successCallback(response) { //success
+      //message
+    }, function errorCallback(response) { //error
+      //message
+    });
+  }
+}
+
+angular
+  .module('app')
+  .service('TagsService', TagsService);
