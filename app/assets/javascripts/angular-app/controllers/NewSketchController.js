da9558b1f31207eda5676bba6258d47f320f79a1
Mitul Mistry <mitulmistrydev@gmail.com>
2016-06-20 16:49:24 +0000

    moved createSketch function from controller to SketchesService

--- app/assets/javascripts/angular-app/controllers/NewSketchController.js
+++ app/assets/javascripts/angular-app/controllers/NewSketchController.js
@@ -4,35 +4,7 @@ function NewSketchController($scope, Upload, $timeout, SketchesService, TagsServ
   ctrl.sketch = {};
 
   ctrl.createSketch = function(){
-    //SketchesService.createSketch(ctrl.sketch);
-
-    var file = ctrl.picFile;
-
-    file.upload = Upload.upload({
-      url: '/sketches',
-      method: 'POST',
-      data: {
-        sketch: { //structured to match rails strong params in sketches controller
-          image: file,
-          title: ctrl.sketch.title,
-          description: ctrl.sketch.description//,
-          //tags: []
-        }
-      }
-    });
-
-    file.upload.then(function (response) {
-      $timeout(function () {
-        file.result = response.data;
-      });
-    }, function (response) {
-      if (response.status > 0)
-      $scope.errorMsg = response.status + ': ' + response.data;
-    }, function (evt) {
-      // Math.min is to fix IE which reports 200% sometimes
-      file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
-    });
-
+    SketchesService.createSketchWithForm(ctrl.picFile, ctrl.sketch, $scope.errorMsg);
   }
 }
 
--- app/assets/javascripts/angular-app/services/SketchesService.js
+++ app/assets/javascripts/angular-app/services/SketchesService.js
@@ -1,4 +1,4 @@
-function SketchesService($http, $state) {
+function SketchesService($http, $state, Upload, $timeout) {
   this.getSketches = function () { //get all the sketches from the serialized controller action
     return $http.get('/sketches.json');
   }
@@ -7,30 +7,33 @@ function SketchesService($http, $state) {
     return $http.get('/sketches/' + id + '.json');
   }
 
-  this.createSketch = function(sketch) {
-    var data = {
-      sketch: { //rails strong params expects sketch model
-        //user_id: Auth._currentUser.id,
-        title: sketch.title,
-        description: sketch.description//,
-        //tags: [],
-        //image:
-      }
-    };
-
-    $http({
-      method: 'POST',
-      dataType: 'json',
+  this.createSketchWithForm = function(file, sketch, errorMsg) {
+    file.upload = Upload.upload({
       url: '/sketches',
-      data: data,
-      headers: {
-        "Content-Type": "application/json"
+      method: 'POST',
+      data: {
+        sketch: { //structured to match rails strong params in sketches controller
+          image: file,
+          title: sketch.title,
+          description: sketch.description//,
+          //tags: []
+        }
       }
-    }).then(function successCallback(response) { //success
-      $state.go('home.sketch', {id: response.data.id }); //go to created sketch
-      //message
-    }, function errorCallback(response) { //error
-      //message
+    });
+
+    file.upload.then(function (response) { //sucess
+      $timeout(function () {
+        file.result = response.data;
+      });
+
+      $state.go('home.sketch', { id: response.data.id }); //on success, rails controller sends back json data of the created sketch, then redirect based on that id
+      //message?
+    }, function (response) { //error
+      if (response.status > 0)
+      errorMsg = response.status + ': ' + response.data; //$scope.errorMsg - not sure if this works being in the service
+    }, function (evt) { //progress
+      // Math.min is to fix IE which reports 200% sometimes
+      file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
     });
   }
 
