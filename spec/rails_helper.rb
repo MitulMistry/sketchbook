7979e9bc67b11cecd9c5dc5f5990a81761c53bf4
MitulMistry <MitulMistryDev@gmail.com>
2017-02-11 00:08:35 -0600

    Set up testing configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,4 +54,26 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Include Factory Girl syntax to simplify calls to factories
+  config.include FactoryGirl::Syntax::Methods
+
+  # Include to enable Devise authentication in tests
+  config.include Devise::Test::ControllerHelpers, type: :controller
+
+  Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    # with.test_framework :minitest
+    # with.test_framework :minitest_4
+    # with.test_framework :test_unit
+
+    # Choose one or more libraries:
+    # with.library :active_record
+    # with.library :active_model
+    # with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    with.library :rails
+  end
 end
