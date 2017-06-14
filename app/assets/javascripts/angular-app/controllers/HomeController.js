cf4175f7f6fd833fe265b154d42fac0f5734ead5
MitulMistry <MitulMistryDev@gmail.com>
2017-06-13 20:08:37 -0500

    Center the footer and add copyright year

--- app/assets/javascripts/angular-app/controllers/HomeController.js
+++ app/assets/javascripts/angular-app/controllers/HomeController.js
@@ -1,6 +1,7 @@
 function HomeController($state) {
   var ctrl = this;
   ctrl.$state = $state;
+  ctrl.date = new Date();
 
   ctrl.home = function(){ //checks whether the current page is home
     return $state.current.name === 'home';
