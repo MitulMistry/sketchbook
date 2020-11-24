9b7f4bcddc0c489a6da6962a38f5ad13f822fc3a
Mitul Mistry <MitulMistryDev@gmail.com>
2020-11-24 12:25:12 -0600

    Update to Rails 6.0.3.4 and update Ruby version to 2.6.5

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2016_06_15_200207) do
     t.datetime "updated_at", null: false
     t.string "image_file_name"
     t.string "image_content_type"
-    t.integer "image_file_size"
+    t.bigint "image_file_size"
     t.datetime "image_updated_at"
   end
 
