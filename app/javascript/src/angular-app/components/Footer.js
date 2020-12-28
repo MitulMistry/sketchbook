950e62e36cc73c7aabedcf735c538f0bae07545f
Mitul Mistry <MitulMistryDev@gmail.com>
2020-12-27 18:45:39 -0600

    Replace ng-include for navbar and footer with components to work with Webpack

---
+++ app/javascript/src/angular-app/components/Footer.js
@@ -0,0 +1,17 @@
+import footerTemplate from '../../templates/footer.html';
+
+var FooterComponent = { //defining an angular component (through setting an object) - Could also use a directive
+  template: footerTemplate, //sets html template to be used
+  bindings: { //define arguments being passed to component - bindings for components, scope for directives
+    data: '=' //set data to a variable being passed in, i.e. when used: <item id="post"></item>
+  },
+  controller: function($state) { //can add functionality to a controller for this component by injecting dependencies such as a service
+    var ctrl = this;
+  },
+  controllerAs: 'footer' //sets controller to be refered to as footer in the template
+};
+
+angular
+  .module('app') //within module 'app'
+  // .component('footer', {template: footer}); //defining this angular component - lowercase string name because no capitalization in DOM element when this component is used
+  .component('footercomponent', FooterComponent); //defining this angular component - lowercase string name because no capitalization in DOM element when this component is used
