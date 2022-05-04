843ddee52ee68e65cdb23574b817e28a15874afd
Mitul Mistry <MitulMistryDev@gmail.com>
2022-05-04 16:54:26 -0500

    Update files with rails app:update command

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
-require 'active_storage/engine'
+require "rails/all"
+require "active_storage/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -12,6 +12,13 @@ module Sketchbook
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    # Configuration for the application, engines, and railties goes here.
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
